f2cb2785df4be2c6afe724ee1d08795d263bc3e5
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-03-30 10:23:50 +0500

    Testing Spring Security with JUnit 5

--- src/test/java/springFrameworkSpringBoot/controllers/BeerControllerTest.java
+++ src/test/java/springFrameworkSpringBoot/controllers/BeerControllerTest.java
@@ -25,6 +25,7 @@ import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -157,6 +158,7 @@ class BeerControllerTest {
                 .willReturn(beerServiceImpl.listBeers(null, null, false, null, null));
 
         mockMvc.perform(get(BeerController.BEER_PATH)
+                        .with(httpBasic("techScale", "TechSc@l3"))
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
